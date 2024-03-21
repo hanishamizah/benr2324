76a313b8c68b2564fd3566b3cad3c84d77719279
hanis <hanishamizahr@gmail.com>
2024-03-21 15:40:26 +0800

    refresh attempt

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json())
 
 app.get('/', (req, res) => {
-   res.send('Hello World!')
+   res.send('BERR 2434 Database and Cloud')
 })
 
 app.listen(port, () => {
